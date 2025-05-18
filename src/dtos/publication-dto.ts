75b77573a8dd398faf3a14af4f79763e4381c34f
João Vitor Krampe Schwening <joaovitorkrampe@gmail.com>
2025-05-17 22:10:12 -0300

    Alterando o import do express no publication dto

--- src/dtos/publication-dto.ts
+++ src/dtos/publication-dto.ts
@@ -1,5 +1,5 @@
 import { Publication } from '@prisma/client';
-import type { Express } from 'express';
+import type Express from 'express';
 
 export interface publicationRequestDto {
   media: Express.Multer.File;
