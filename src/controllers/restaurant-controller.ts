e25ff3a8e2be78c49ea654d32b5616d5229a232f
Nicholas Spolti <nicholasspoltidesouza@gmail.com>
2025-05-23 18:56:25 -0300

    fix: add km comparation

--- src/controllers/restaurant-controller.ts
+++ src/controllers/restaurant-controller.ts
@@ -51,7 +51,11 @@ export class RestaurantController {
       name: req.query.name?.toString(),
       address: req.query.address?.toString(),
       geolocation: geo && geo.length === 2 ? [geo[0], geo[1]] : undefined,
-      proximity: req.query.proximity ? Number(req.query.proximity) : undefined,
+      proximity: req.query.proximity
+        ? Number(req.query.proximity)
+        : req.query.address
+          ? 5
+          : undefined,
       price_range: req.query.price_range
         ? Number(req.query.price_range)
         : undefined,
